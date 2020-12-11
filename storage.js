321d5445be50e7e391b97e2245a1344ace44eeb6
Andrew <andrewadams3@mail.fresnostate.edu>
2020-12-10 23:32:15 -0800

    use session storage instead

--- storage.js
+++ storage.js
@@ -1,10 +1,10 @@
 function storeCredentials(creds) {
-  if (typeof creds === "string") localStorage.setItem("creds", creds);
-  else localStorage.setItem("creds", JSON.stringify(creds));
+  if (typeof creds === "string") sessionStorage.setItem("creds", creds);
+  else sessionStorage.setItem("creds", JSON.stringify(creds));
 }
 
 function getCredentials() {
-  let creds = localStorage.setItem("creds");
+  let creds = sessionStorage.setItem("creds");
   if (creds.length > 0) return JSON.parse(creds);
   else return {};
 }
